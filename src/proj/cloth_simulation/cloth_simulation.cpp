32b99c20437c0038c465b0baf56ecba86cc37b1a
Hiyoung.Tsui <992896924@qq.com>
2018-05-10 11:10:39 +0800

    rebuild

--- src/proj/cloth_simulation/cloth_simulation.cpp
+++ src/proj/cloth_simulation/cloth_simulation.cpp
@@ -13,8 +13,8 @@
 
 #include <iostream>
 
-#include "imgui.h"
-#include "imgui_impl_glfw_gl3.h"
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw_gl3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
